dc5438d43c64263b76e7acb23fc83aed29280931
Gaurav Mishra <gm3669317@gmail.com>
2024-05-17 10:02:53 +0530

    dsfrh

---
+++ AlzyCareRepo/MemoryLaneViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MemoryLaneViewController.swift
+//  AlzyCareRepo
+//
+//  Created by Batch-2 on 17/05/24.
+//
+
+import UIKit
+
+class MemoryLaneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
