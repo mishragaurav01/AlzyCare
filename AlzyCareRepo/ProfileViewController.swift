07440619270a400004543ff134627dc2ff2842dd
Gaurav Mishra <gm3669317@gmail.com>
2024-05-17 12:45:11 +0530

    Horizontal Scroll Added category.

---
+++ AlzyCareRepo/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  AlzyCareRepo
+//
+//  Created by Batch-2 on 17/05/24.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
