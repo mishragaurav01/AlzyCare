90f14c1fd7a2cca359784d40bb45c09a73e3a071
Rishita <rishitakumari206@gmail.com>
2024-05-17 12:14:05 +0530

    add Photo button function added

---
+++ AlzyCareRepo/PhotoAlbum1ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoAlbum1ViewController.swift
+//  AlzyCareRepo
+//
+//  Created by Batch-2 on 17/05/24.
+//
+
+import UIKit
+
+class PhotoAlbum1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
